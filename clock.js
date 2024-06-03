dd7ee19ba6edd2e619cb59a96af297ed3e81cc4b
Ume-Aham Sylvester <117973007+gittybro12@users.noreply.github.com>
2024-06-03 12:28:37 +0100

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,19 @@
+function myClock(){
+    let d = new Date()
+
+    document.getElementById("clock").innerHTML = 
+    d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+
+    let h = document.getElementById("clock")
+    h.style.backgroundColor = "black"
+    h.style.color = 'white'
+    h.style.width = 'fit-content'
+    h.style.padding= "100px"
+    h.style.borderRadius = '40%'
+    h.style.position = 'absolute'
+    h.style.top = '35%'
+    h.style.left = '40%'
+}
+
+setInterval(myClock,1000)
